4fa950cef54f9996de7132088e713b86e67925e6
Pierre Mousnier <pierre@mybizdev.io>
2022-06-03 16:00:49 +0200

    Start create model

--- lgm-le-wagon/ml_logic/model.py
+++ lgm-le-wagon/ml_logic/model.py
@@ -16,96 +16,102 @@ from typing import Tuple
 
 import numpy as np
 
+def model():
+    #Predict if Out of office
+    pass
 
-def initialize_model(X: np.ndarray) -> Model:
-    """
-    Initialize the Neural Network with random weights
-    """
-    print(Fore.BLUE + "\nInitialize model..." + Style.RESET_ALL)
-
-    reg = regularizers.l1_l2(l2=0.005)
-
-    model = Sequential()
-    model.add(layers.BatchNormalization(input_shape=X.shape[1:]))
-    model.add(layers.Dense(100, activation="relu", kernel_regularizer=reg))
-    model.add(layers.BatchNormalization())
-    model.add(layers.Dropout(rate=0.1))
-    model.add(layers.Dense(50, activation="relu", kernel_regularizer=reg))
-    model.add(layers.BatchNormalization())
-    model.add(layers.Dropout(rate=0.1))
-    model.add(layers.Dense(10, activation="relu"))
-    model.add(layers.BatchNormalization(momentum=0.99))  # use momentum=0 for to only use statistic of the last seen minibatch in inference mode ("short memory"). Use 1 to average statistics of all seen batch during training histories.
-    model.add(layers.Dropout(rate=0.1))
-    model.add(layers.Dense(1, activation="linear"))
-
-    print("\n✅ model initialized")
-
-    return model
+########################### TAXIFARE #####################################
 
 
-def compile_model(model: Model, learning_rate: float) -> Model:
-    """
-    Compile the Neural Network
-    """
-    optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
-    model.compile(loss="mean_squared_error", optimizer=optimizer, metrics=["mae"])
+# def initialize_model(X: np.ndarray) -> Model:
+#     """
+#     Initialize the Neural Network with random weights
+#     """
+#     print(Fore.BLUE + "\nInitialize model..." + Style.RESET_ALL)
 
-    print("\n✅ model compiled")
-    return model
+#     reg = regularizers.l1_l2(l2=0.005)
 
+#     model = Sequential()
+#     model.add(layers.BatchNormalization(input_shape=X.shape[1:]))
+#     model.add(layers.Dense(100, activation="relu", kernel_regularizer=reg))
+#     model.add(layers.BatchNormalization())
+#     model.add(layers.Dropout(rate=0.1))
+#     model.add(layers.Dense(50, activation="relu", kernel_regularizer=reg))
+#     model.add(layers.BatchNormalization())
+#     model.add(layers.Dropout(rate=0.1))
+#     model.add(layers.Dense(10, activation="relu"))
+#     model.add(layers.BatchNormalization(momentum=0.99))  # use momentum=0 for to only use statistic of the last seen minibatch in inference mode ("short memory"). Use 1 to average statistics of all seen batch during training histories.
+#     model.add(layers.Dropout(rate=0.1))
+#     model.add(layers.Dense(1, activation="linear"))
 
-def train_model(model: Model,
-                X: np.ndarray,
-                y: np.ndarray,
-                batch_size=256,
-                validation_split=0.3,
-                validation_data=None) -> Tuple[Model, dict]:
-    """
-    Fit model and return a the tuple (fitted_model, history)
-    """
+#     print("\n✅ model initialized")
 
-    print(Fore.BLUE + "\nTrain model..." + Style.RESET_ALL)
-
-    es = EarlyStopping(monitor="val_loss",
-                       patience=2,
-                       restore_best_weights=True,
-                       verbose=0)
-
-    history = model.fit(X,
-                        y,
-                        validation_split=validation_split,
-                        validation_data=validation_data,
-                        epochs=100,
-                        batch_size=batch_size,
-                        callbacks=[es],
-                        verbose=0)
-
-    print(f"\n✅ model trained ({len(X)} rows)")
-
-    return model, history
+#     return model
 
 
-def evaluate_model(model: Model,
-                   X: np.ndarray,
-                   y: np.ndarray,
-                   batch_size=256) -> Tuple[Model, dict]:
-    """
-    Evaluate trained model performance on dataset
-    """
+# def compile_model(model: Model, learning_rate: float) -> Model:
+#     """
+#     Compile the Neural Network
+#     """
+#     optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
+#     model.compile(loss="mean_squared_error", optimizer=optimizer, metrics=["mae"])
 
-    print(Fore.BLUE + f"\nEvaluate model on {len(X)} rows..." + Style.RESET_ALL)
+#     print("\n✅ model compiled")
+#     return model
 
-    metrics = model.evaluate(
-        x=X,
-        y=y,
-        batch_size=batch_size,
-        verbose=1,
-        # callbacks=None,
-        return_dict=True)
 
-    loss = metrics["loss"]
-    mae = metrics["mae"]
+# def train_model(model: Model,
+#                 X: np.ndarray,
+#                 y: np.ndarray,
+#                 batch_size=256,
+#                 validation_split=0.3,
+#                 validation_data=None) -> Tuple[Model, dict]:
+#     """
+#     Fit model and return a the tuple (fitted_model, history)
+#     """
 
-    print(f"\n✅ model evaluated: loss {round(loss, 2)} mae {round(mae, 2)}")
+#     print(Fore.BLUE + "\nTrain model..." + Style.RESET_ALL)
 
-    return metrics
+#     es = EarlyStopping(monitor="val_loss",
+#                        patience=2,
+#                        restore_best_weights=True,
+#                        verbose=0)
+
+#     history = model.fit(X,
+#                         y,
+#                         validation_split=validation_split,
+#                         validation_data=validation_data,
+#                         epochs=100,
+#                         batch_size=batch_size,
+#                         callbacks=[es],
+#                         verbose=0)
+
+#     print(f"\n✅ model trained ({len(X)} rows)")
+
+#     return model, history
+
+
+# def evaluate_model(model: Model,
+#                    X: np.ndarray,
+#                    y: np.ndarray,
+#                    batch_size=256) -> Tuple[Model, dict]:
+#     """
+#     Evaluate trained model performance on dataset
+#     """
+
+#     print(Fore.BLUE + f"\nEvaluate model on {len(X)} rows..." + Style.RESET_ALL)
+
+#     metrics = model.evaluate(
+#         x=X,
+#         y=y,
+#         batch_size=batch_size,
+#         verbose=1,
+#         # callbacks=None,
+#         return_dict=True)
+
+#     loss = metrics["loss"]
+#     mae = metrics["mae"]
+
+#     print(f"\n✅ model evaluated: loss {round(loss, 2)} mae {round(mae, 2)}")
+
+#     return metrics
