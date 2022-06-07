7520c7ebee39a8806c1b8542540ae49e77c5ebbe
sami boudraa <sami.boudraa.16@neoma-bs.com>
2022-06-07 19:02:31 +0200

    api en cours

--- api/fast.py
+++ api/fast.py
@@ -7,6 +7,10 @@ import joblib
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from api.helper import *
+
+
+
 app = FastAPI()
 
 app.add_middleware(
@@ -17,6 +21,7 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
+#url = 'http://127.0.0.1:8000/predict'
 
 
 @app.get("/")
@@ -24,25 +29,78 @@ def index():
     return dict(greeting="hello")
 
 @app.get("/predict")
-def predict(log_type,
+def predict(type,
             reply,
             first_message,
             _id):
 
+    if type == "GOOGLE_REPLY":   #GOOGLE_REPLY = mail
+        return dict(
+        type=['email detected'],
+        reply=[float(reply)],
+        first_message=[float(first_message)],
+        _id=[float(_id)])
+        return GOOGLE_REPLY.workflow # TODO
+
+
+
+    elif type == "LINKEDIN_HAS_REPLY":  #LINKEDIN_HAS_REPLY=linkedin
+        return dict(
+        type=['linkedin message detected'],
+        reply=[float(reply)],
+        first_message=[float(first_message)],
+        _id=[float(_id)])
+        return LINKEDIN_HAS_REPLY.workflow # TODO
+
     return dict(
-        log_type=[log_type],
+        type=['type not detected'],
         reply=[float(reply)],
         first_message=[float(first_message)],
         _id=[float(_id)])
 
+
+
+
+
+    @app.get("/predict")
+    def predict(type,
+            reply,
+            first_message,
+            _id):
+
+        if type == "GOOGLE_REPLY":   #GOOGLE_REPLY = mail
+            if is_ooo(reply)==True:
+                pass
+            else:
+                sales_pred = sales_sentiment(reply)
+
+            return {"log_id":_id, "identity": "sales", "sentiment":sales_pred}
+
+        else:
+            if is_rh(reply)==True:
+                identity = "rh"
+                rh_pred = rh_sentiment(reply)
+            else:
+                identity = "sales"
+                sales_pred = sales_sentiment(reply)
+
+            return {"log_id":_id, "identity":identity, "sentiment":sales_pred}
+
+
+
+
+
     X = pd.DataFrame(dict(       # vérifier correspondance clées
         key=[key],
-        log_type=[log_type],
+        type=[type],
         reply=[float(reply)],
         first_message=[float(first_message)],
         _id=[float(_id)]))
 
-# http://127.0.0.1:8000/predict?log_type=0000&reply=0001&first_message=0002&_id=0003
+# http://127.0.0.1:8000/predict?type=GOOGLE_REPLY&reply=0001&first_message=0002&_id=0003
+# http://127.0.0.1:8000/predict?type=LINKEDIN_HAS_REPLY&reply=0001&first_message=0002&_id=0003
+# http://127.0.0.1:8000/predict?type=0000&reply=0001&first_message=0002&_id=0003
+
 
     # pipeline = get_model_from_gcp()
     pipeline = joblib.load('model.joblib')  #vérifier correspondance nom du modele
@@ -54,3 +112,10 @@ def predict(log_type,
     pred = float(results[0])
 
     return dict(fare=pred)
+"""
+@app.post
+def predict(type,
+            reply,
+            first_message,
+            _id):
+            """
