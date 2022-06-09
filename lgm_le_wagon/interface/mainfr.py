3d1c2c8632f8abe09a34d3035900147a99cc53b3
Artus Granier <artus.granier@live.fr>
2022-06-09 11:35:51 +0200

    queqlues modifs, eval etc

--- lgm_le_wagon/interface/mainfr.py
+++ lgm_le_wagon/interface/mainfr.py
@@ -55,20 +55,75 @@ def preprocess_and_train_SAFR():
 
     # print(f"\n✅ trained on {row_count} rows ({cleaned_row_count} cleaned) with accuracy {round(val_accuracy, 2)}")
 
-    return val_accuracy
+    return val_accuracy, model
 
-# def predict_SAFR():
+def evaluate_SAFR(model):
 
-#     #Tokenize our test
-#     inputs_ids_test, input_masks_test = tokenize(X_FR_test, tokenizer)
+    df = get_storage_data(task="Sentiment_FR")
+    X_FR = df["reply"]
+    y_FR = df[["negative", "neutral", "positive"]]
+    X_FR_train, X_FR_test, y_FR_train, y_FR_test = train_test_split(X_FR, y_FR, test_size=0.3)
 
-#     #evaluate our model
-#     evaluate_model(model, [inputs_ids_test, input_masks_test] ,y_FR_test, batch_size=32)
+    tokenizer = create_tokenizer_fr()
+    inputs_ids_test, input_masks_test = tokenize(test, tokenizer)
 
+    #Tokenize our test
+    inputs_ids_test, input_masks_test = tokenize(X_FR_test, tokenizer)
+
+    #evaluate our model
+    evaluate_model(model, [inputs_ids_test, input_masks_test] ,y_FR_test, batch_size=32)
+
+    #     # save evaluation
+    # params = dict(
+    #     # package behavior
+    #     context="evaluate",
+    #     # data source
+    #     first_row=first_row,
+    #     row_count=len(X_new_processed))
+
+    # save_model(params=params, metrics=metrics_dict)
+    #loss = metrics["loss"]
+    #accuracy = metrics["accuracy"]
+    #mae = metrics_dict["mae"]
+
+    return print('evaluated')
+
+def pred(
+    X_pred: pd.DataFrame = None
+    , stage="None"
+) -> np.ndarray:
+    """
+    Make a prediction using the latest trained model
+    """
+
+    print("\n⭐️ use case: predict")
+
+    # if X_pred is None:
+
+        # X_pred = pd.DataFrame(dict(
+        #     key=["2013-07-06 17:18:00"],  # useless but the pipeline requires it
+        #     pickup_datetime=["2013-07-06 17:18:00 UTC"],
+        #     pickup_longitude=[-73.950655],
+        #     pickup_latitude=[40.783282],
+        #     dropoff_longitude=[-73.984365],
+        #     dropoff_latitude=[40.769802],
+        #     passenger_count=[1]))
+
+    model = load_model(
+        stage=stage
+    )
+
+    X_processed = preprocess_features(X_pred)
+
+    y_pred = model.predict(X_processed)
+
+    print("\n✅ prediction done: ", y_pred, y_pred.shape)
+
+    return y_pred
 
 if __name__ == '__main__':
     preprocess_and_train_SAFR()
     #preprocess()
     #train()
     #pred()
-    #evaluate(first_row=9000)
+    evaluate_SAFR()
